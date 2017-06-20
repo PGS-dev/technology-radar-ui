ac499de0b856ce0fa89d5cff7018b7cee777db3c
1323 <jlobaza7@o2.pl>
2017-06-20 11:06:57 +0200

    Main page link name is based on current path now.

--- src/navbar/RadarNav.js
+++ src/navbar/RadarNav.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+const RadarNav = ({match}) => {
 
-const RadarNav = ({match}) => (
+    const routesArray = [['11IUPvEX2RJ_ZoNMQeSVo7ghj2-BpeTCUIG3KoMf7Ifc', 'Frontend Radar'],
+    ['18Wg-5N7qOnEr1sbSx2f_Yh90kTYNAxnpW7ZHE_9orQg', 'Example Radar']];
+
+    function checkUrl(match, routesArray) {
+          const routeName = routesArray.find(route => (match.url.indexOf(route[0])!==-1));
+          return routeName ? routeName[1] : 'Tech Radar';
+    }
+    const navHomeLink = checkUrl(match, routesArray);
+ 
+    return (
     <span>
-        <Link to={{pathname: `/${match.params.spreadsheetId}`}}>Technology radar</Link>
+        <Link to={{pathname: `/`}}>{navHomeLink}</Link>
         {/*<Link to={{pathname: `/${match.params.spreadsheetId}/blips`}}>All blips</Link>*/}
     </span>
-);
+    )
+};
 
 export default RadarNav;
