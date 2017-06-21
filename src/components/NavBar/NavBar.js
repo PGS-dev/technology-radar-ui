b9dbd89ab905ecc9d67750a73dbc8095e8f01c89
1323 <jlobaza7@o2.pl>
2017-06-21 14:40:26 +0200

    path fix

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import { dataService } from '../DataService';
+import { dataService } from '../../services/DataService/DataService';
 import './NavBar.css';
 
 import RadarNav from "./RadarNav";
