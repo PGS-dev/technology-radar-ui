fffd7344c5c8edf6bd639db695b456868ac55f9c
Michal Hans (PGS Software) <mhans@pgs-soft.com>
2017-07-07 13:01:38 +0200

    Added D3 chart

--- src/components/Radar.vue
+++ src/components/Radar.vue
@@ -1,20 +1,75 @@
 <template>
-
-  <section>
-    [radar goes here]
-  </section>
-
+    <svg id="radar" class="scaling-svg" viewBox="0 0 1000 1000"></svg>
 </template>
 
 <script>
+  import Chart from './../assets/chart'
+
   export default {
     name: 'radar',
+    props: [
+      'radarData'
+    ],
     data () {
       return {}
+    },
+    mounted: function () {
+      const clickHandler = {}
+      let radar = new Chart(this.$el, this.radarData, clickHandler)
+      console.log(radar)
+    },
+    watch: {
+      radarData: function () {
+        const clickHandler = {}
+        this.$el.innerHTML = ''
+        let radar = new Chart(this.$el, this.radarData, clickHandler)
+        console.log(radar)
+      }
     }
   }
 </script>
 
-<style scoped>
+<style>
+  .scaling-svg-container {
+    position: relative;
+    height: 0;
+    width: 100%;
+    padding: 0;
+    padding-bottom: 100%;
+  }
 
+  .scaling-svg {
+    max-height: 80vh;
+  }
+
+  .itemLabel, .legendLabel, .areaLabel {
+    font-family: 'Oswald', sans-serif;
+    opacity: 1;
+    transform-origin: top right;
+    -moz-transform-origin: 0 0;
+  }
+
+  .itemLabel:hover {
+    font-weight: bold;
+  }
+
+  .itemLabel {
+    cursor: pointer;
+  }
+
+  .areaFill {
+    opacity: 0.5;
+  }
+
+  .legendLabel{
+    fill: white;
+  }
+
+  .legendArcBg {
+    fill: #F57C00;
+  }
+
+  .areaLabelArc {
+    fill: none;
+  }
 </style>
--- src/components/SnapshotPage.vue
+++ src/components/SnapshotPage.vue
@@ -1,15 +1,15 @@
 <template>
   <section>
     <md-tabs md-fixed class="md-transparent">
+      <md-tab id="chart" md-label="Chart">
+        <h2 style="position: absolute">{{snapshotId}}</h2>
+        <radar v-if="currentSnapshot && currentSnapshot.blips" :radar-data="currentSnapshot"></radar>
+      </md-tab>
+
       <md-tab id="table" md-label="Table">
         <h2>{{snapshotId}}</h2>
         <p v-if="currentSnapshot.blips">Blips: {{currentSnapshot.blips.length}}</p>
         <blips-table v-if="currentSnapshot.blips" :blips="currentSnapshot.blips"></blips-table>
-      </md-tab>
-
-      <md-tab id="chart" md-label="Chart">
-        <h2>{{snapshotId}}</h2>
-        [CHART]
       </md-tab>
     </md-tabs>
   </section>
@@ -18,11 +18,13 @@
 <script>
   import {mapState} from 'vuex'
   import BlipsTable from './BlipsTable'
+  import Radar from './Radar'
 
   export default {
     name: 'snapshotPage',
     components: {
-      'blipsTable': BlipsTable
+      'blipsTable': BlipsTable,
+      'radar': Radar
     },
     props: [
       'spreadsheetId',
