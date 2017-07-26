b33e25eeb81312f88534d2940ee5bd3d4dd88640
Michal Hans (PGS Software) <mhans@pgs-soft.com>
2017-07-26 21:43:17 +0200

    Table filtering by blip name

--- src/components/BlipsTable.vue
+++ src/components/BlipsTable.vue
@@ -3,17 +3,20 @@
     <md-table>
       <md-table-header>
         <md-table-row>
-          <md-table-head>Name</md-table-head>
+          <md-table-head>
+            <input class="filter" placeholder="Name..." v-model="nameFilter"/>
+          </md-table-head>
           <md-table-head>Section</md-table-head>
           <md-table-head>Status</md-table-head>
           <md-table-head>Previous</md-table-head>
           <md-table-head>State</md-table-head>
         </md-table-row>
       </md-table-header>
-
       <md-table-body>
-        <md-table-row v-for="blip in blips" :key="blip.name">
-          <md-table-cell><router-link :to="`/${spreadsheetId}/blip/${blip.name}`">{{blip.name}}</router-link></md-table-cell>
+        <md-table-row v-for="blip in filteredBlips" :key="blip.name">
+          <md-table-cell>
+            <router-link :to="`/${spreadsheetId}/blip/${blip.name}`">{{blip.name}}</router-link>
+          </md-table-cell>
           <md-table-cell>{{blip.section}}</md-table-cell>
           <md-table-cell>{{blip.status}}</md-table-cell>
           <md-table-cell>{{blip.previousStatus}}</md-table-cell>
@@ -30,18 +33,49 @@
     props: [
       'blips',
       'spreadsheetId'
-    ]
+    ],
+    data: () => {
+      return {
+        nameFilter: ''
+      }
+    },
+    computed: {
+      filteredBlips: (vm) => {
+        let query = vm.nameFilter.toLowerCase()
+        if (query.length) {
+          return vm.blips.filter((blip) => blip.name.toLowerCase().indexOf(query) >= 0)
+        } else {
+          return vm.blips
+        }
+      }
+    }
   }
 </script>
 
 <style scoped>
   .tableContainer {
-    background: rgba(255,255,255, 0.5);
+    background: rgba(255, 255, 255, 0.5);
     max-width: 900px;
     margin: 0 auto;
   }
 
   .tableContainer .md-table tbody .md-table-row {
-    border-top-color: rgba(255,255,255,0.1)
+    border-top-color: rgba(255, 255, 255, 0.1)
+  }
+
+  .tableContainer .md-table tbody .md-table-row:nth-child(1) {
+    background: #eee;
+  }
+
+  input.filter {
+    border: none;
+    background: none;
+    font-weight: bold;
+    font-size: 12px;
+    color: rgba(0, 0, 0, 0.54);
+  }
+
+  input:focus {
+    outline: none;
   }
 </style>
